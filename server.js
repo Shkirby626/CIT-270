06dc77370e5e6d5ef2024bc6fe97ad9270391129
Scott Kirby <kir17002@byui.edu>
2022-12-12 10:58:11 -0700

    made changes

--- server.js
+++ server.js
@@ -3,27 +3,34 @@ const bodyparser = require("body-parser");
 const https = require('https')
 const fs = require('fs')
 const {v4 : uuidv4} = require("uuid");
-const port = 4043;
+const port = 443;
 const app = express();
 const {createClient} = require('redis');
 const md5 = require('md5');
+const Max_login = 3;
 
 const redisClient = createClient(
 {
-    Url:'redis://default@localhost:6379',
+    url:`redis://default:${process.env.REDIS_PASS}@redis-stedi-scott:6379`,
 }
 );
 
 app.use(bodyparser.json());
 
+app.use(express.static("public"))
+
 https.createServer({
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.cert'),
-    ca: fs.readFileSync('chain.pem')
+    key: fs.readFileSync('./SSL/server.key'),
+    cert: fs.readFileSync('./SSL/server.cert'),
+    ca: fs.readFileSync('./SSL/chain.pem')
     
 }, app).listen(port, async () => {
-    await redisClient.connect();
-    console.log('Listening...')
+    try{
+        await redisClient.connect();
+        console.log('Listening...')}
+    catch(error){
+        console.log(error)
+    }
 });
 
 // app.listen(port, async ()=>{
@@ -46,7 +53,10 @@ app.post('/user', (req,res)=>{
     redisClient.hSet('users', req.body.email, JSON.stringify(newUserRequestObject));
     res.send('NEW USER ' +newUserRequestObject.email+' ADDED');
 });
+var login_dictionary = {};
+
 app.post("/login", async (req,res) =>{
+    const newUserRequestObject = req.body;
     const loginEmail = req.body.userName;
     console.log (JSON.stringify(req.body));
     console.log("loginEmail", loginEmail);
@@ -55,6 +65,10 @@ app.post("/login", async (req,res) =>{
 
     const userString=await redisClient.hGet('users',loginEmail);
     const userObject=JSON.parse(userString)
+    if(login_dictionary[loginEmail] == null) {login_dictionary[loginEmail] = 0}
+    if(login_dictionary[loginEmail] >= Max_login){
+        return res.status(469).send("Too many failed attempts")
+    }
     if(userString =='' || userString==null){
         res.status(404);
         res.send('User not found');
@@ -64,6 +78,8 @@ app.post("/login", async (req,res) =>{
         const token = uuidv4();
         res.send(token);
     } else{
+        login_dictionary[loginEmail] += 1;
+        redisClient.hset("users", req.loginEmail,JSON.stringify(userObject));
         res.status(401);//unauthorized
         res.send("Invalid user or password");
     }
